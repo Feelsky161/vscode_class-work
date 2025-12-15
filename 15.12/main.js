c434b3a16fab16c26654c8f0294ccba4d55b3d16
Feelsky161 <ggooggll03@gmail.com>
2025-12-15 21:13:07 +0300

    123

---
+++ 15.12/main.js
@@ -0,0 +1,95 @@
+// let num1 = prompt("введите 1 число")
+// let num2 = prompt("введите 2 число")
+// alert(num1+"+"+num2+"+"+num1+num2)
+// alert вывод
+// prompt
+// console.log
+// typeof
+// let num1 = 
+// let num2 =
+// let num3 = num1+num2
+// console.log(num3)
+// alert(num3)
+// let userName="Илья"
+// console.log(`ваше имя,${userName}`)
+// name=prompt("введите имя: ")
+// result = confirm
+// let num1 = 89
+// num1 = "Вася"
+
+// num1 = +prompt("введите число 1")
+// alert(num1*num1)
+
+// let num1 = +prompt("Введите 1 число");
+// let num2 = +prompt("Введите 2 число");
+// alert((num1 + num2) / 2);
+
+
+// let num1 = +prompt("Введите 1 число");
+// let num2 = +prompt("Введите 2 число");
+// alert(num1*num2);
+
+// if (age>18){
+//     console.log("welcome")
+// }
+// else{
+//     console.log("ERROR")
+// }
+
+/*
+and &&
+or ||
+not !
+===
+==
+??
+
+*/
+// let num1 = +prompt("Введите 1 число");
+// let num2 = +prompt("Введите 2 число");
+// let num3 = +prompt("Введите 3 число");
+// let num4 = +prompt("Введите 4 число");
+// let num5 = +prompt("Введите 5 число");
+// let maxNumber = Math.max(num1, num2, num3, num4, num5);
+//     console.log(`Наибольшее число: ${maxNumber}`);
+
+
+// let a = +prompt("Введите 1 число");
+// let b = +prompt("Введите 2 число");
+//     console.log(`Числа в диапазоне от ${a} до ${b}:`);
+//     let c = a;
+//     while (c <= b) {
+//         console.log(c);
+//         c++;
+//     }
+
+// for(создаём переменные; условия; что сделать после итерации)
+// for(i=0; i<10; i++){
+//     console.log(i)
+// }
+
+// a = +prompt("Введите 1 число");
+//  for(i=a; i>=0; i++){
+//      console.log(i)
+//  }
+
+
+// a=1
+// switch(a){
+//     case 1:
+//         console.log("1")
+//         break
+
+//     case 2:
+//         console.log("2")
+//         break
+
+//     case 3:
+//         console.log("3")
+//         break
+// }
+
+// function summa(a,b){
+//     return a+b;
+// }
+// console.log(summa(3,7))
